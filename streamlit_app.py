34497de990589f8a4e82fd36bd6c1103c6c1959c
johege <139977663+johege@users.noreply.github.com>
2024-10-17 23:58:32 +0000

    adapted app

--- streamlit_app.py
+++ streamlit_app.py
@@ -102,6 +102,15 @@ def main():
         "dayMaxEvents": True,
         "weekNumbers": True,
         "navLinks": True,
+        "windowResize": """
+            function(view) {
+                if (window.innerWidth < 768) {  # Anpassung für Smartphones
+                    view.calendar.changeView('dayGridWeek');  # Wochenansicht für kleine Bildschirme
+                } else {
+                    view.calendar.changeView('dayGridMonth');  # Monatsansicht für größere Bildschirme
+                }
+            }
+        """
     }
 
     
